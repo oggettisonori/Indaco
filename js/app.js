797d528df6b644418790df838437a4dfe5231f0c
oggettisonori <oggettisonori@gmail.com>
2017-10-23 17:20:10 +0200

    fix

--- js/app.js
+++
@@ -1,6 +0,0 @@
-import { CustomMPCElement } from './mpc-element.js';
-import { CustomPadElement } from './pad-element.js';
-
-
-customElements.define('x-mpc', CustomMPCElement);
-customElements.define('x-pad', CustomPadElement);
